7b90b91b212de0ecad0c4bf32924c7e299cfbe73
Moriyuki SUZUKI <moriyukisuzuki74@gmail.com>
2025-10-15 08:17:02 +0000

    feat: add HeroUI integration and configure Vitest for testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HeroUIProvider } from "@heroui/react";
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HeroUIProvider>
+        <App />
+    </HeroUIProvider>
   </React.StrictMode>,
 );
